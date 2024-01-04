1db33c920d9741ffb6a3d5599f5ad077552030dc
Hardik Sarkar <hardiksarkar198@gmail.com>
2024-01-04 21:25:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ShoppingCart from './components/ShoppingCart'
+import Header from './components/Header'
+import CartPage from './components/CartPage'
 
-function App() {
+export default function App() {
+  let [home,setHome] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header home={home} setHome={setHome}/>
+      {home?<ShoppingCart/>:<CartPage/>}
     </div>
-  );
+  )
 }
-
-export default App;
